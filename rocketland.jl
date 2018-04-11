07d811fda07d066a83547aff63723668609a6022
Benjamin Chung <ckfinite@gmail.com>
2018-04-10 21:44:43 -0400

    quicksave

--- rocketland.jl
+++ rocketland.jl
@@ -1,24 +1,18 @@
 using JuMP
 using Mosek
-using DifferentialEquations
-using ForwardDiff
 using Rotations
 using StaticArrays
 
 #state indexing
 const state_dim = 14
 const control_dim = 3
-const mass_idx = 1
-const r_idx = SVector(2,3,4)
-const v_idx = SVector(5,6,7)
-const qbi_idx = SVector(8,9,10,11)
-const omb_idx = SVector(12,13,14)
 const r_idx_it = 2:4
 const v_idx_it = 5:7
 const qbi_idx_it = 8:11
 const omb_idx_it = 12:14
 const acc_width = state_dim+control_dim*2+3
 const acc_height = state_dim
+const mass_idx = 1
 
 type DescentProblem
     g::Float64
@@ -70,17 +64,13 @@ struct ProbInfo
 end
 
 struct LinPoint
-    state::MArray{Tuple{14}, Float64, 1, 14}
-    control::MArray{Tuple{3}, Float64, 1, 3}
+    state::SArray{Tuple{14}, Float64, 1, 14}
+    control::SArray{Tuple{3}, Float64, 1, 3}
 end
 
 struct LinRes
-    Ak::Array{Float64,2}
-    Bk::Array{Float64,2}
-    Ck::Array{Float64,2}
-    Sigk::Array{Float64,1}
-    Zk::Array{Float64,1}
-    iter_end::Array{Float64, 1} #dunno
+    endpoint::SArray{Tuple{14},Float64,1,14}
+    derivative::SArray{Tuple{21,14},Float64,2,294}
 end
 
 type ProblemIteration
@@ -90,127 +80,10 @@ type ProblemIteration
     dynam::Array{LinRes,  1}
 end
 
-struct IntegratorParameters
-    dt :: Float64
-    uk :: MArray{Tuple{3}, Float64, 1, 3}
-    up :: MArray{Tuple{3}, Float64, 1, 3}
+if !isdefined(:Dynamics)
+    include("dynamics.jl")
 end
-
-function DCM(quat::SArray{Tuple{4}, T, 1, 4} where T)
-    q0 = quat[1]
-    q1 = quat[2]
-    q2 = quat[3]
-    q3 = quat[4]
-
-    return transpose(@SMatrix [(1-2*(q2^2 + q3^2))         (2*(q1 * q2 + q0 * q3))  (2*(q1*q3 - q0 * q2));
-                    (2*(q1 * q2 - q0 * q3))     (1-2*(q1^2 + q3^2))      (2*(q2*q3 + q0 * q1));
-                    (2*(q1 * q3 + q0 * q2))     (2*(q2*q3 - q0 * q1))    (1-2*(q1^2 + q2^2)) ])
-end
-
-function Omega{T}(omegab::SArray{Tuple{3}, T, 1, 3})
-    z = zero(T)
-    return @SMatrix [z        -omegab[1] -omegab[2] -omegab[3];
-                    omegab[1] z           omegab[3] -omegab[2];
-                    omegab[2] -omegab[3]  z          omegab[1];
-                    omegab[3]  omegab[2] -omegab[1]  z         ]
-end
-
-function dx(state::SArray{Tuple{14}, T, 1, 14} where T, u::MArray{Tuple{3}, T, 1, 3} where T, info::ProbInfo)
-    qbi = state[qbi_idx]
-    omb = state[omb_idx]
-    thr_acc = DCM(qbi) * u/state[mass_idx]
-    grv_acc = @SVector [-info.g0,0,0]
-    acc = thr_acc + grv_acc
-    rot_vel = 0.5*Omega(omb)*qbi
-    rot_acc = info.jBi*(cross(info.rTB,u) - cross(omb,info.jB*omb))
-    return @SVector [-info.a*norm(u), 
-            state[5],state[6],state[7], 
-            acc[1], acc[2], acc[3], 
-            rot_vel[1], rot_vel[2], rot_vel[3], rot_vel[4], 
-            rot_acc[1], rot_acc[2], rot_acc[3]]
-end
-
-function compute_A(state::SArray{Tuple{14}, T, 1, 14} where T, u::MArray{Tuple{3}, T, 1, 3} where T, target::MArray{Tuple{14,14}, T, 2, 196} where T, info::ProbInfo)
-    ForwardDiff.jacobian!(target, istate -> dx(istate, u, info), state)
-end
-
-function compute_B(state::SArray{Tuple{14}, T, 1, 14} where T, u::MArray{Tuple{3}, T, 1, 3} where T, target::MArray{Tuple{14,3}, T, 2, 42} where T, info::ProbInfo)
-    ForwardDiff.jacobian!(target, iu -> dx(state, iu, info), u)
-end
-
-function linearize_dynamics(states::Array{LinPoint,1}, sigma_lin::Float64, dt::Float64, info::ProbInfo)
-    #preallocate memory
-    const Aval = @MArray zeros(state_dim, state_dim)
-    const Bval = @MArray zeros(state_dim, control_dim)
-    const clin = @MVector zeros(control_dim)
-    const cneg = @MVector zeros(control_dim)
-    const cpls = @MVector zeros(control_dim)
-
-    #compute indices into the combined accumulator matrix
-    const state_idx = 1
-    const A_start = state_idx + 1
-    const A_end = A_start + state_dim - 1
-    const B_start = A_end + 1
-    const B_end = B_start + control_dim - 1
-    const C_start = B_end + 1
-    const C_end = C_start + control_dim - 1
-    const Sig_idx = C_end + 1
-    const Z_idx = Sig_idx + 1
-
-    #integrand worker
-    #the accumulator is laid out as 
-    # state:1 |     Ak:state_dim     | Bkm:control_dim | Bkp:control_dim | sigK:1 | zk:1
-    # all are state_dim high. Dims are thus state_dim+control_dim*2+3 by state_dim.
-    function integrand(du,
-                       u,#::MArray{Tuple{acc_height,acc_width}, Float64, 2, acc_height*acc_width},
-                       p::IntegratorParameters,
-                       t::Float64)
-        lkm = (p.dt-t)/p.dt
-        lkp = t/p.dt
-        broadcast!(*, cneg, lkm, p.uk)
-        broadcast!(*, cpls, lkp, p.up)
-        broadcast!(+, clin, cneg, cpls)
-
-        sv = SVector{14}(u[:,state_idx])
-        du[:,1] = dx(sv, clin, info)
-        compute_A(sv, clin, Aval, info)
-        compute_B(sv, clin, Bval, info)
-        broadcast!(*, Aval, sigma_lin, Aval)
-        broadcast!(*, Bval, sigma_lin, Bval)
-        phi = u[:,A_start:A_end]
-        iPhi = inv(phi)
-
-        du[:,A_start:A_end] = Aval*phi
-        du[:,B_start:B_end] = lkm*(iPhi * Bval)
-        du[:,C_start:C_end] = lkp*(iPhi * Bval)
-        du[:,Sig_idx] = iPhi*(du[:,1])
-        du[:,Z_idx] = iPhi*(-Aval*u[:,1] - Bval * clin)
-    end
-
-
-    results = Array{LinRes,1}(length(states)-1)
-    for i=1:length(states)-1
-        uk = states[i].control
-        up = states[i+1].control
-        int_iv = hcat(states[i].state, eye(Float64, state_dim), zeros(Float64, state_dim, control_dim*2 + 2))
-        prob = ODEProblem(integrand, int_iv, (0.0,dt), IntegratorParameters(dt, uk, up))
-        sol = DifferentialEquations.solve(prob, DP5(); dtmin=0.001, force_dtmin=true)
-        result = sol.u[end]
-
-        Ak::Array{Float64,2} = result[:,A_start:A_end]
-        Bkm::Array{Float64,2} = Ak*result[:,B_start:B_end]
-        Bkp::Array{Float64,2} = Ak*result[:,C_start:C_end]
-        Sigk::Array{Float64,1} = Ak*result[:,Sig_idx]
-        zk::Array{Float64,1} = Ak*result[:,Z_idx]
-        results[i] = LinRes(Ak, 
-            Bkm, 
-            Bkp, 
-            Sigk, 
-            zk, 
-            result[:,1])
-    end
-    return results
-end
+using Dynamics
 
 function create_initial(problem::DescentProblem)
     K = problem.K
@@ -221,10 +94,10 @@ function create_initial(problem::DescentProblem)
         vIk = (K+1-k)/(K+1) * problem.vIi + (k/(K+1))*problem.vIf
 
         rot = rotation_between([1,0,0], -vIk)
-        qBIk = [rot.w, rot.x, rot.y, rot.z]
-        TBk = mk*problem.g*[1,0,0]
-        state_init = vcat(mk,rIk,vIk,qBIk,[0.0,0,0])
-        control_init = [mk*problem.g,0,0]
+        qBIk = @SVector [rot.w, rot.x, rot.y, rot.z]
+        TBk = @SVector [mk*problem.g,0,0]
+        state_init = vcat(mk,rIk,vIk,qBIk,(@SVector [0.0,0,0]))
+        control_init = @SVector [mk*problem.g,0,0]
         initial_points[k] = LinPoint(state_init, control_init)
     end
     linpoints = linearize_dynamics(initial_points, problem.tf_guess, 1.0/(K+1), ProbInfo(problem))
@@ -237,6 +110,7 @@ function solve_step(iteration::ProblemIteration)
     K = iteration.problem.K
     iterDynam = iteration.dynam
     iterAbout = iteration.about
+    sigHat = iteration.sigma
 
     tggs = tand(prob.gammaGs)
     sqcm = sqrt((1-cosd(prob.thetaMax))/2)
@@ -275,7 +149,11 @@ function solve_step(iteration::ProblemIteration)
     #dynamics
     for i=1:K
         dk = iterDynam[i]
-        @constraint(m, x[:,i+1] .== dk.Ak * x[:,i] + dk.Bk * u[:,i] + dk.Ck * u[:,i+1] + dk.Sigk*sigma + dk.Zk + nu[:, i])
+        ab = iterAbout[i]
+        abn = iterAbout[i+1]
+        ctrl = hcat(vcat(x[:,i]-ab.state, u[:,i]-ab.control, u[:,i+1]-abn.control,sigma-sigHat))'
+        println(dk.derivative)
+        @constraint(m, x[:,i+1] .== Array{Float64,2}(dk.derivative) *ctrl + dk.endpoint)
     end
     for i=1:K+1
         #state constraints
